9aa99b93c2f10bbc0189e46cd0bed61d7bcf982e
Zac Bergquist <zbergquist99@gmail.com>
2016-09-29 17:52:07 -0600

    Fix tests, update package description

--- spec/tester-spec.js
+++ spec/tester-spec.js
@@ -75,7 +75,7 @@ describe('tester', () => {
       let testBuffer = testEditor.getBuffer()
       testBuffer.setText('package main\n\nimport "testing"\n\nfunc TestHello(t *testing.T) {\n\tresult := Hello()\n\tif result != "Hello, 世界" {\n\t\tt.Errorf("Expected %s - got %s", "Hello, 世界", result)\n\t}\n}')
       testBuffer.save()
-      let p = mainModule.getTester().runCoverage(editor)
+      let p = mainModule.getTester().runTests(editor)
 
       waitsForPromise(() => { return p })
 
@@ -108,7 +108,7 @@ describe('tester', () => {
         expect(range.end.column).toBe(1)
       })
 
-      p = mainModule.getTester().runCoverage(editor)
+      p = mainModule.getTester().runTests(editor)
 
       waitsForPromise(() => { return p })
 
@@ -157,7 +157,7 @@ describe('tester', () => {
       let testBuffer = testEditor.getBuffer()
       testBuffer.setText('package main\n\nimport "testing"\n\nfunc TestHello(t *testing.T) {\n\tresult := Hello()\n\tif result != "Hello, 世界" {\n\t\tt.Errorf("Expected %s - got %s", "Hello, 世界", result)\n\t}\n}')
       testBuffer.save()
-      let p = mainModule.getTester().runCoverage(editor)
+      let p = mainModule.getTester().runTests(editor)
 
       waitsForPromise(() => { return p })
 
